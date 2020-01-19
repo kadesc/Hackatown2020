6529eaa08d33961db2cff2640a41a4af56e0e671
unknown <sara.brochu@polymtl.ca>
2020-01-19 02:15:28 -0500

    finished version of remote image api

--- computer_vision_api.py
+++ computer_vision_api.py
@@ -11,73 +11,65 @@ import sys
 import time
 
 
-# Add your Computer Vision subscription key to your environment variables.
-if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
-    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-else:
-    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
-# Add your Computer Vision endpoint to your environment variables.
-if 'COMPUTER_VISION_ENDPOINT' in os.environ:
-    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-else:
-    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
+def makeAPI(remote_image_url):
+    MINIMAL_CONFIDENCE = 70
 
-computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
-remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+    if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+        subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+    else:
+        print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
+    if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+        endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+    else:
+        print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
 
-
-
-'''
-Describe an Image - remote
-This example describes the contents of an image with the confidence score.
-'''
-print("===== Describe an image - remote =====")
-# Call API
-description_results = computervision_client.describe_image(remote_image_url )
-
-# Get the captions (descriptions) from the response, with confidence level
-print("Description of remote image: ")
-if (len(description_results.captions) == 0):
-    print("No description detected.")
-else:
-    for caption in description_results.captions:
-        print("'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100))
+    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 
 
-
-'''
-Categorize an Image - remote
-This example extracts (general) categories from a remote image with a confidence score.
-'''
-print("===== Categorize an image - remote =====")
-# Select the visual feature(s) you want.
-remote_image_features = ["categories"]
-# Call API with URL and features
-categorize_results_remote = computervision_client.analyze_image(remote_image_url , remote_image_features)
-
-# Print results with confidence score
-print("Categories from remote image: ")
-if (len(categorize_results_remote.categories) == 0):
-    print("No categories detected.")
-else:
-    for category in categorize_results_remote.categories:
-        print("'{}' with confidence {:.2f}%".format(category.name, category.score * 100))
+    image_tags = []
+    
+    '''Describes the contents of a remote image with the confidence score'''   
+    description_results = computervision_client.describe_image(remote_image_url )  # Call API
+    if (len(description_results.captions) == 0):
+        print("No description detected.")
+    else:
+        for caption in description_results.captions:
+            if ((caption.confidence * 100) >= MINIMAL_CONFIDENCE):
+                image_tags.append([caption.text, caption.confidence * 100])
 
 
-'''
-Tag an Image - remote
-This example returns a tag (key word) for each thing in the image.
-'''
-print("===== Tag an image - remote =====")
-# Call API with remote image
-tags_result_remote = computervision_client.tag_image(remote_image_url )
+    '''Describes the category of a remote image with the confidence score'''   
+    remote_image_features = ["categories"]
+    categorize_results_remote = computervision_client.analyze_image(remote_image_url , remote_image_features)     # Call API with URL and features
+    if (len(categorize_results_remote.categories) == 0):
+        print("No categories detected.")
+    else:
+        for category in categorize_results_remote.categories:
+            if ((category.score * 100) >= MINIMAL_CONFIDENCE):
+                image_tags.append([category.name, category.score * 100])
 
-# Print results with confidence score
-print("Tags in the remote image: ")
-if (len(tags_result_remote.tags) == 0):
-    print("No tags detected.")
-else:
-    for tag in tags_result_remote.tags:
-        print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+
+
+    '''
+    Returns a tag (key word) for each thing in the image.
+    '''
+    tags_result_remote = computervision_client.tag_image(remote_image_url )     # Call API with remote image
+    if (len(tags_result_remote.tags) == 0):
+        print("No tags detected.")
+    else:
+        for tag in tags_result_remote.tags:
+            if ((tag.confidence * 100) >= MINIMAL_CONFIDENCE):
+                image_tags.append([tag.name, tag.confidence * 100])
+    
+
+
+    for i in range(len(image_tags)):
+        for j in range(len(image_tags)):
+            if image_tags[i][1] > image_tags[j][1]:
+                image_tags[i],image_tags[j] = image_tags[j],image_tags[i]
+
+
+
+    return image_tags
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import testhackatown
+from computer_vision_api import makeAPI
 
 if __name__ == "__main__":
+    image_URL = 'https://www.wsetglobal.com/media/7108/1608x900_wine_bottles.jpg'
+    image_tags = makeAPI(image_URL)
+    
+    liste_dechets = "liste_dechets.txt"
+    
