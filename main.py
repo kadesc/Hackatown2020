7b3e57db7ea0074369348e0debdedcd1e7dcf892
unknown <sara.brochu@polymtl.ca>
2020-01-19 11:31:58 -0500

    modified main

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ from image_recognition_api import extractNameTags, makeImageRecognitionAPI, find
 
 if __name__ == "__main__":
     file_names = ['trash.txt','recyclable.txt','compost.txt','other.txt']
+    image_tags = []
 
-    '''Create tags'''
-
-    
-    # User written input
-    user_tag = []
-
-    # Image input
-    user_input = input("Enter directory or url: ")
-    image_location = 'https://www.wsetglobal.com/media/7108/1608x900_wine_bottles.jpg' #url or directory (path)
-    image_location_mode = "remote"
-    image_tags = makeImageRecognitionAPI(image_location_mode,image_location)
-
+    '''Create search tags'''
+    search_mode = input("Enter corresponding number to desired search mode:\n 1 - text, 2 - local image, 3- online image\n")
+    if (search_mode == "2"):
+        search_mode = "local"
+        image_location = input("Enter directory: ")
+        image_tags = makeImageRecognitionAPI(search_mode,image_location)
+    elif (search_mode == "3"):
+        search_mode = "remote"
+        image_location = input("Enter url: ")
+        image_tags = makeImageRecognitionAPI(search_mode,image_location)
+    elif (search_mode == "1"):
+        image_tags = input("Enter item: ")
+        image_tags = [image_tags]
+    else:
+        print("Not a correct number entered")
     #Search tags in database and print result
     search_results = findInDatabase(file_names, image_tags)
-    print(search_results)
+    if (len(search_results)!= 0):
+        print(search_results)
+    else:
+        print("No result found")
+        
     input("\nPress enter...")
