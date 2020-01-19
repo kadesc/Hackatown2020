415f9dcfd0b2b469569ab2e6578c45db2bf8e6eb
Jenseth Fuentes <jenseth_fuentes@hotmail.com>
2020-01-19 06:39:46 -0500

    GUI python file update

--- trashGUI.py
+++ trashGUI.py
@@ -1,36 +1,78 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
+# -*- coding: utf-8 -*-
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(0, 0, 1920, 1080)
-        self.setWindowTitle("Trash")
-        self.initUI()
-    def initUI(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("My First Label")
-        self.label.move (1000, 500) 
+# Form implementation generated from reading ui file 'deTRItus.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
 
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("Click me")
-        self.b1.clicked.connect(self.clicked)
-    
-    def clicked(self):
-        self.label.setText("You pressed the button")
-        self.update()
-    
-    def update(self):
-        self.label.adjustSize()
 
-def clicked():
-    print("clicked")
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-def window():
-    app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
-    sys.exit(app.exec())
 
-window()
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(596, 429)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(60, 0, 131, 81))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(30, 60, 291, 241))
+        self.graphicsView.setObjectName("graphicsView")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 350, 56, 17))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(190, 350, 56, 17))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(30, 320, 291, 20))
+        self.textBrowser.setObjectName("textBrowser")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(370, 60, 211, 111))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setGeometry(QtCore.QRect(370, 190, 211, 51))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.textEdit_3 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_3.setGeometry(QtCore.QRect(370, 260, 211, 81))
+        self.textEdit_3.setObjectName("textEdit_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 596, 18))
+        self.menubar.setObjectName("menubar")
+        self.menufile = QtWidgets.QMenu(self.menubar)
+        self.menufile.setAutoFillBackground(False)
+        self.menufile.setObjectName("menufile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menufile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "deTRItus"))
+        self.pushButton.setText(_translate("MainWindow", "URL"))
+        self.pushButton_2.setText(_translate("MainWindow", "Manuel"))
+        self.menufile.setTitle(_translate("MainWindow", "file"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
